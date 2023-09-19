30fcf43d103640b07dc1746bda04b263330cc0f0
guenguen97 <guenguen9797@gmail.com>
2023-09-19 09:25:07 +0900

    거의 완료

--- src/main/resources/static/js/all.js
+++ src/main/resources/static/js/all.js
@@ -8,7 +8,7 @@ anchors: ["section1", "section2", "section3", "section4"],
 //navigation 동그라미 버튼 페이지
 navigation: true,
 navigationPosition: "left",
-navigationTooltips: ["section1", "section2", "section3", "section4"],
+navigationTooltips: ["소개", "색상 선택", "옵션 선택", "section4"],
 //navigation 이름을 계속 뜸
 showActiveTooltip: true,
 slidesNavigation: true,
@@ -16,6 +16,15 @@ slidesNavigation: true,
 loopBottom: true,
 //첫번째 페이지 스크롤하면 마지막페이지로 이동
 loopTop: true,
+
+  afterLoad: function(origin, destination, direction) {
+      // Change tooltip colors based on the active section
+      if (destination.anchor === "section1") {
+        document.querySelector("#fp-nav ul li a span").style.color = "white";
+      } else if (destination.anchor === "section2" || destination.anchor === "section3") {
+        document.querySelector(`#fp-nav ul li[data-tooltip="${destination.anchor}"] a span`).style.color = "black";
+      }
+    }
 });
 
 
@@ -25,4 +34,5 @@ new fullpage('#fullpage', {
   // scrollOverflow 옵션 활성화
   scrollOverflow: true,
 });
+
 
