4feaf2eb26f92f40cc1244d2eee360fdb9b8afe5
guenguen97 <guenguen9797@gmail.com>
2023-09-20 13:48:28 +0900

    dasd

--- src/main/java/com/project/carPoor/SeleniumConfig.java
+++ src/main/java/com/project/carPoor/SeleniumConfig.java
@@ -1,8 +1,10 @@
 package com.project.carPoor;
 
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +14,13 @@ public class SeleniumConfig {
 
     @Bean
     public WebDriver webDriver() {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver"); // 크롬 드라이버 경로 설정
+        ChromeOptions options = new ChromeOptions();
+        // Chrome 드라이버 경로 설정
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        // Chrome 브라우저를 headless 모드로 실행 (화면에 띄우지 않음)
+        options.addArguments("--headless");
+
+
         return new ChromeDriver();
     }
 }
--- src/main/java/com/project/carPoor/controller/PdfConversionController.java
+++ src/main/java/com/project/carPoor/controller/PdfConversionController.java
@@ -3,7 +3,11 @@ package com.project.carPoor.controller;
 import com.project.carPoor.service.PdfConversionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-
-
+@Controller
+@RequiredArgsConstructor
+@Component
 public class PdfConversionController {
 
     @Autowired
-    private PdfConversionService pdfConversionService;
+    private final PdfConversionService pdfConversionService;
 
 
     @PostMapping("/generatePdf")
@@ -26,7 +31,7 @@ public class PdfConversionController {
         // PDF 생성 로직을 호출하고, 생성된 PDF 파일을 반환 또는 다운로드할 수 있는 방법
         // 예: PdfConversionService를 호출하여 PDF를 생성하고 파일 경로를 반환
         // 그 후 클라이언트에게 파일 다운로드 링크를 전달하거나 PDF를 표시할 수 있음
-        String outputPath = "C:\\path\\output.pdf";
+        String outputPath = "C:/output.pdf";
         try {
             pdfConversionService.convertHtmlToPdf(url, outputPath);
         } catch (IOException e) {
@@ -37,6 +42,21 @@ public class PdfConversionController {
         // PDF 생성 로직은 PdfConversionService에 있어야 함
 
         return ResponseEntity.ok(outputPath); // PDF 다운로드 링크로 리다이렉트
+//        try {
+//            // PDF 생성 로직 호출 및 PDF 파일 바이트 배열로 가져오기
+//            byte[] pdfBytes = pdfConversionService.convertHtmlToPdf(url);
+//
+//            // PDF 파일을 클라이언트에게 반환
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.setContentType(MediaType.APPLICATION_PDF);
+//            headers.setContentDispositionFormData("attachment", "output.pdf"); // 다운로드할 파일 이름 설정
+//            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//        } finally {
+//            pdfConversionService.closeWebDriver();
+//        }
     }
 
     @RequestMapping("/downloadPdf")
