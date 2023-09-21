195b9c35af07d297ef7fe5af39b44a04edb2f990
unknown <you@example.com>
2023-09-21 12:48:20 +0900

    자동차pdf 완성

--- src/main/resources/static/js/money.js
+++ src/main/resources/static/js/money.js
@@ -38,6 +38,35 @@
                                                                         	    return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g,'$1');
                                                                         	}
 
+                                //마이페이지  내 차 들의 견적서에서 가격 표시 하는거
+
+                                                                 $(document).ready(function() {
+
+                                                                     var $checkElements = $('.main-box2-body-container');
+
+                                                                      // 각 .check 요소에 대한 작업을 수행합니다.
+                                                                      $checkElements.each(function() {
+                                                                        // 현재 .check 요소 내에서 .price 클래스를 찾습니다.
+                                                                        var $priceElement = $(this).find(".myPage-price2");
+                                                                        var money4 = $priceElement.text();
+                                                                        console.log(money4+"머니4");
+
+                                                                        // .price2 클래스를 가진 요소를 찾습니다.
+                                                                        var $money5 = $(this).find(".myPage-price1");
+
+                                                                        // 숫자 형식을 변환하여 콤마를 추가합니다.
+                                                                        var money6 = money4.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                                                                        console.log(money6);
+
+                                                                        // 변환된 값을 .price2 클래스를 가진 요소의 텍스트로 설정합니다.
+                                                                        $money5.text(money6+"원");
+                                                                      });
+                                                                        });
+
+
+
+
+
 
                                    // section2 에서  옵션 여러개 나오는 가격들에 콤마 붙이는 함수
 
